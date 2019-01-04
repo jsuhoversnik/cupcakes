d0c49dcdb958bb5c62eb1ef648f267f5c7f2e310
jsuhoversnik <jake.suhoversnik@gmail.com>
2019-01-03 20:20:17 -0500

    changed flavors to display via array

--- index.php
+++ index.php
@@ -5,8 +5,13 @@
     <title>Cupcakes</title>
 </head>
 <body>
-<?PHP
-$flavors = array("grasshopper" => "The Grasshopper",
+
+    <form action="">
+        <label for="name">Name: </label>
+        <input type="text" id="name">
+        <li>
+            <?PHP
+            $flavors = array("grasshopper" => "The Grasshopper",
                 "maple" => "Whiskey Maple Bacon",
                 "carrot" => "Carrot Walnut",
                 "caramel" => "Salted Caramel Cupcake",
@@ -14,24 +19,12 @@ $flavors = array("grasshopper" => "The Grasshopper",
                 "lemon" => "Lemon Drop",
                 "tiramisu" => "Tiramisu");
 
-//echo $flavors[0];
-//echo $flavors['grasshopper'];
 
-?>
+            foreach($flavors as $key => $value){
+                echo "<ul><input type=\"checkbox\" value=\"" . $key . "\">". $value ."</ul>";
+            }
 
-
-
-    <form action="">
-        <label for="name">Name: </label>
-        <input type="text" id="name">
-        <li>
-            <ul><input type="checkbox" value=''>The Grasshopper</ul>
-            <ul><input type="checkbox" value="maple">Whiskey Maple Bacon</ul>
-            <ul><input type="checkbox" value="carrot">Carrot Walnut</ul>
-            <ul><input type="checkbox" value="caramel">Salted Caramel Cupcake</ul>
-            <ul><input type="checkbox" value="velvet">Red Velvet</ul>
-            <ul><input type="checkbox" value="lemon">Lemon Drop</ul>
-            <ul><input type="checkbox" value="tiramisu">Tiramisu</ul>
+            ?>
         </li>
 
         <button type="submit">Submit</button>
